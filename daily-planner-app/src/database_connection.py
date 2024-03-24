dbfbdda13db977d037af09d67920fb85cb78b8b1
Riku <134082872+rigozu9@users.noreply.github.com>
2024-03-24 17:07:03 +0200

    added tests for register, sign in and adding an activity

--- daily-planner-app/src/database_connection.py
+++ daily-planner-app/src/database_connection.py
@@ -1,11 +1,16 @@
 import os
 import sqlite3
-"""Tietokantaan connection."""
+
 dirname = os.path.dirname(__file__)
 
-connection = sqlite3.connect(os.path.join(dirname, "..", "data", "database.sqlite"))
-connection.row_factory = sqlite3.Row
+# Determine if we are running in a test environment
+is_test_env = os.getenv("TEST_ENV", "False") == "True"
 
+# Choose database file based on the environment
+db_file = "test_database.sqlite" if is_test_env else "database.sqlite"
+
+connection = sqlite3.connect(os.path.join(dirname, "..", "data", db_file))
+connection.row_factory = sqlite3.Row
 
 def get_database_connection():
     return connection
