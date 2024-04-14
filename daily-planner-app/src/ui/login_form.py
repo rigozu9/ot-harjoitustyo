7d2caf88807c18e468e306493d02ac1c671de581
Riku <134082872+rigozu9@users.noreply.github.com>
2024-04-14 18:34:46 +0300

    changed how ui views work when logged in

--- daily-planner-app/src/ui/login_form.py
+++ daily-planner-app/src/ui/login_form.py
@@ -1,8 +1,10 @@
 """tkinter import, messagebox for error dailyplanner and surveyview"""
 import tkinter as tk
 from tkinter import messagebox
+from datetime import date
 from ui.daily_planner_view import DailyPlanner
 from ui.survey_view import SurveyView
+from ui.today_view import TodayView
 
 class LoginForm:
     """"Sisäänkirjatumisen form. Kysyy nimeä ja salasanaa.
@@ -12,6 +14,8 @@ class LoginForm:
         self._frame = tk.Frame(self._master)
         self._user_service = user_service
         self._daily_plan_service = daily_plan_service
+
+        self._date = date.today()
 
         self._username_label = tk.Label(self._frame, text="Username:")
         self._username_label.pack()
@@ -39,10 +43,16 @@ class LoginForm:
         password = self._password_entry.get()
 
         user_id = self._user_service.login_user(username, password)
+        plans = self._daily_plan_service.get_plans_by_id(user_id, self._date)
         if user_id:
             self._frame.destroy()
             first_login = self._user_service.is_first_login(user_id)
-            if first_login:
+            if plans:
+                TodayView(self._master,
+                    user_id,
+                    self._user_service,
+                    self._daily_plan_service)
+            elif first_login:
                 SurveyView(self._master,
                            user_id,
                            self._user_service,
